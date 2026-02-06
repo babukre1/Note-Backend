af10972b78aa3ff5159bd2e2ac5e9497b28913e7
Abubakr <abubakrwindowz@gmail.com>
2026-02-06 17:47:37 +0300

    added edit my profile endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -45,7 +45,9 @@ router.post("/signup", async (req, res) => {
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
   try {
-    let result = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+    let result = await pool.query("SELECT * FROM users WHERE username = $1", [
+      username,
+    ]);
 
     if (result.rows.length === 0) {
       return res.status(400).json({ msg: "Invalid Credentials" });
@@ -59,17 +61,16 @@ router.post("/login", async (req, res) => {
     }
 
     // Assign actual values to session
-    req.session.user = { 
-      id: userData.id, 
+    req.session.user = {
+      id: userData.id,
       username: userData.username,
-      fullname: userData.fullname 
+      fullname: userData.fullname,
     };
 
     req.session.save((err) => {
       if (err) return res.status(500).json({ msg: "Session save failed" });
       return res.json({ msg: "Logged in successfully" });
     });
-    
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server error");
@@ -102,3 +103,18 @@ router.get("/logout", (req, res) => {
 });
 
 module.exports = router;
+
+
+router.put("/editMyProfile", auth, async (req, res) => {
+  const { fullname, username } = req.body;
+  try {
+    const updatedUser = await pool.query(
+      "UPDATE users SET fullname = $1, username = $2 WHERE id = $3 RETURNING id, username, fullname, role",
+      [fullname, username, req.user.id], // req.user.id comes from your adminAuth middleware
+    );
+    res.json({ msg: "Profile updated", user: updatedUser.rows[0] });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
